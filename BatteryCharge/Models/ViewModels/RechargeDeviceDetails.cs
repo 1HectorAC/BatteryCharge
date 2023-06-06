389c921a4fe82b7d53adf20bef09b64ea9377d65
Hector Acosta <1Hector.AC@gmail.com>
2023-06-06 15:00:15 -0700

    Added RechargeDeviceDetails model.

---
+++ BatteryCharge/Models/ViewModels/RechargeDeviceDetails.cs
@@ -0,0 +1,17 @@
+﻿namespace BatteryCharge.Models.ViewModels
+{
+    public class RechargeDeviceDetails
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public DateTime LastRechargeDate { get; set; }
+
+        public int RechargeCycle { get; set; }
+
+        public int DaysSinceLastRecharge { get; set; }
+
+
+    }
+}
